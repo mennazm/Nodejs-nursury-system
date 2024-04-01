38bf619494561efc77e94d40eb8efb8300249c34
mennazm <91394925+mennazm@users.noreply.github.com>
2024-04-01 20:09:00 +0200

    finalisa

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,14 +1,72 @@
+// const jwt = require("jsonwebtoken");
+
+// module.exports = (request, response, next) => {
+//     try {
+//         let token = request.headers.authorization.split(" ")[1];
+       
+//         console.log("Token:", token); // Log the token
+//         let decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+//         console.log("Decoded Token:", decodedToken); 
+//         request.token = decodedToken;
+//        // request.id = decodedToken.id;
+//         // request.role = decodedToken.role;
+      
+//         console.log(request.token);
+
+//         next();
+//     } catch(error) {
+//         console.error("JWT Error:", error);
+//         error.status = 401;
+//         error.message = "You are not authorized to access";
+//         next(error);
+//     }
+// };
+
+// // auth.js
+
+// // Middleware for checking if the user is admin
+// module.exports.checkAdmin = (request, response, next) => {
+//     if (request.token.role === "admin") {
+//         next();
+//     } else {
+//         let error = new Error("You don't have admin privileges.");
+//         error.status = 403;
+//         next(error);
+//     }
+// };
+
+// // Middleware for checking if the user is admin or teacher
+// module.exports.checkAdminOrTeacher = (request, response, next) => {
+//     if (request.token.role === "admin" || request.token.role === "teacher") {
+//         next();
+//     } else {
+//         let error = new Error("You don't have permission for this action");
+//         error.status = 403;
+//         next(error);
+//     }
+// };
+
+
 const jwt = require("jsonwebtoken");
 
+// Middleware for JWT token verification
 module.exports = (request, response, next) => {
     try {
-        let token = request.headers.authorization.split(" ")[1];
+        const authorizationHeader = request.headers.authorization;
+        if (!authorizationHeader) {
+            throw new Error("Authorization header missing");
+        }
+        
+        const token = authorizationHeader.split(" ")[1];
+        if (!token) {
+            throw new Error("Token not found in authorization header");
+        }
        
         console.log("Token:", token); // Log the token
-        let decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
         console.log("Decoded Token:", decodedToken); 
         request.token = decodedToken;
-       // request.id = decodedToken.id;
+        // request.id = decodedToken.id;
         // request.role = decodedToken.role;
       
         console.log(request.token);
@@ -17,19 +75,17 @@ module.exports = (request, response, next) => {
     } catch(error) {
         console.error("JWT Error:", error);
         error.status = 401;
-        error.message = "You are not authorized to access";
+        error.message = "Authentication failed: " + error.message;
         next(error);
     }
 };
-
-// auth.js
 
 // Middleware for checking if the user is admin
 module.exports.checkAdmin = (request, response, next) => {
     if (request.token.role === "admin") {
         next();
     } else {
-        let error = new Error("You don't have admin privileges.");
+        const error = new Error("You don't have admin privileges.");
         error.status = 403;
         next(error);
     }
@@ -40,7 +96,7 @@ module.exports.checkAdminOrTeacher = (request, response, next) => {
     if (request.token.role === "admin" || request.token.role === "teacher") {
         next();
     } else {
-        let error = new Error("You don't have permission for this action");
+        const error = new Error("You don't have permission for this action");
         error.status = 403;
         next(error);
     }
